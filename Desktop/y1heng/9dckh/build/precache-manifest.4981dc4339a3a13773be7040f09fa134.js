536e583b537a6d4b96542f88d54445da5a339e63
caobenfei <caobenfei@zhrt.com.cn>
2020-12-31 17:45:00 +0800

    提交1

---
+++ Desktop/y1heng/9dckh/build/precache-manifest.4981dc4339a3a13773be7040f09fa134.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482c2be3aeea0f9ed673b6f0ed31fc94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f0adfaa29900871303f",
+    "url": "/static/css/main.c3cb34f8.chunk.css"
+  },
+  {
+    "revision": "bc3ff80211c5c05c7510",
+    "url": "/static/js/2.b209ffe5.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.b209ffe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f0adfaa29900871303f",
+    "url": "/static/js/main.8b26a87a.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
